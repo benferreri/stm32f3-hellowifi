c7c1d044d9b9c5df5a7dde37377bf8754eb4249c
benferreri <benferreri11@gmail.com>
2020-02-22 17:41:34 -0500

    added functions for communication with esp8266

---
+++ src/wifi.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include "uart.h"
+#include "clock.h"
+
+void sendcmd(uint8_t command[], uint8_t response[], uint32_t responselen) {
+    uint8_t c;
+    uint32_t i = 0;
+    uint32_t j = 1;
+    uart1_send(command);
+    sleep(1000);
+    while ((c = uart1_getchar()) == '\r' || c == '\n')
+        ; // skip
+    while ((c = uart1_getchar()) == command[j++] && c != '\r' && c != '\n')
+        ; // esp8266 echoes the command
+    while ((c = uart1_getchar()) == '\r' || c == '\n')
+        ; // skip
+    /* copy response */
+    response[i++] = c;
+    while (i < responselen - 1 && (c = uart1_getchar()) != '\r' && c != '\n')
+        response[i++] = c;
+    response[i] = '\0';
+}
---
+++ src/wifi.h
@@ -0,0 +1,6 @@
+#ifndef __WIFI_H
+#define __WIFI_H
+
+void sendcmd(uint8_t command[], uint8_t response[], uint32_t responselen);
+
+#endif
